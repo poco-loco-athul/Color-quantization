fcfc00af08053623b6f0724b00791335380d461c
athul <kpathul@outlook.com>
2019-03-02 07:57:02 +0530

    Refactor: pylint optimization

--- color_palette.py
+++ color_palette.py
@@ -8,14 +8,14 @@ from PIL import Image
 
 # if len(sys.argv) == 2:
 #     im = Image.open(sys.argv[1])
-# else:    
+# else:
 #     # opens image
-#     im = Image.open("example.jpg")
-im = Image.open("example.jpg")
+#     picture = image.open("example.jpg")
+picture = Image.open("example.jpg")
 
 
 # get image data
-data = np.array(im.getdata())
+data = np.array(picture.getdata())
 modified_data = copy.deepcopy(data)
 
 
@@ -23,7 +23,7 @@ modified_data = copy.deepcopy(data)
 kmeans = KMeans(n_clusters=5, max_iter=1).fit(data)
 centers = kmeans.cluster_centers_.astype(int)
 
-def color_difference(col1,col2):
+def color_difference(col1, col2):
     "Returns Euclidean distance between 2colors in RGB color space"
     return ((col1[0] - col2[0])**2 +
             (col1[1] - col2[1])**2 +
@@ -33,26 +33,26 @@ def color_difference(col1,col2):
 # Image of color palette
 width, height = 128, 128
 palette = Image.new('RGB', (width*len(centers), height))
-for i in range(len(centers)):
+for i, _ in enumerate(centers):
     col = Image.new("RGB", (width, height), tuple(centers[i]))
-    palette.paste(col, (width*i,0))
+    palette.paste(col, (width*i, 0))
 
 
 # Makes image-data in range of color palette
-for pix in range(len(modified_data)):
+for pix, _ in enumerate(modified_data):
     col_dic = {}
     for col in centers:
-        col_dic[tuple(col)] = color_difference(modified_data[pix],col)
+        col_dic[tuple(col)] = color_difference(modified_data[pix], col)
     for key, value in col_dic.items():
         if min(col_dic.values()) == value:
             modified_data[pix] = key
 
-modified_im = Image.new('RGB', im.size)
+modified_picture = Image.new('RGB', picture.size)
 m_data = list(tuple(pixel) for pixel in modified_data)
-modified_im.putdata(m_data)
+modified_picture.putdata(m_data)
 
 
 if __name__ == "__main__":
-    im.show()
+    picture.show()
     palette.show()
-    modified_im.show()    
+    modified_picture.show()
